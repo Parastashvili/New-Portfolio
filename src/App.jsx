9491dbc1089a30d7331ad289fd30cd6a0d864fb4
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-07-18 14:22:25 +0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import React, { useState, useEffect } from "react";
 import Slider from "../components/Slider";
 import Reveal from "../components/Reveal";
 import Techstack from "../components/TechStack";
+import 'transition-style';
 function App() {
   const [text, setText] = useState("");
   const words = ["Frontend Engineer", "Web Designer"];
